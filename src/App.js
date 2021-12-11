120646d1a7fe9b94edec7481a889c0e6483da2a0
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-12-11 23:26:30 +0100

    Add loading, icons and removed services folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHashtag, faIdBadge, faPalette, faStar, faTags } from '@fortawesome/free-solid-svg-icons';
+
+// STYLES
 import './App.css'
 
 // COMPONENTS
 import AppHeader from './components/AppHeader';
-
-// SERVICES
-import DiscordService from './services/discord-service';
 
 function App () {
 
@@ -13,6 +14,8 @@ function App () {
   const [userID, setUserID] = useState('');
   const [isReady, setIsReady] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const handleChange = (e) => {
     const value = e.target.value;
@@ -21,18 +24,42 @@ function App () {
     if (value === '' || regex.test(value)) {
       setUserID(value);
     }
+
+    if (value.length >= 15) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
   }
 
   const handleClick = () => {
-    if (userID) {
-      DiscordService.getUser(userID).then((user) => {
-        setDiscordUser(user);
-        setIsReady(true);
-      });
-      setIsError(false);
-    } else {
+    fetchUser();
+  }
+
+  const fetchUser = async () => {
+    setIsReady(false);
+    setIsLoading(true);
+    setIsError(false);
+
+    await fetch(`https://discord-lookup-api.herokuapp.com/api/user/profile?q=${userID}`)
+    .then((response) => response.json())
+    .then((response) => {
+      const data = response.data;
+
+      if (!data) {
+        setIsLoading(false);
+        setIsError(true);
+        return;
+      }
+
+      setDiscordUser(data);
+      setIsReady(true);
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      setIsLoading(false);
       setIsError(true);
-    }
+    });
   }
 
   return (
@@ -43,12 +70,11 @@ function App () {
           <div>
             <label htmlFor='userid-input' className="userid-label">User ID / Bot ID:</label>
             <input type="text" name="userid-input" id="userid-input" className="userid-input" maxLength={24} onChange={handleChange} value={userID} />
-            <button id="btn" className="btn btn__info" onClick={handleClick}>Lookup</button>
-            {isError &&
-              <span className='error'>User ID may not be empty</span>
-            }
+            <button id="btn" className="btn btn__info" onClick={handleClick} disabled={isDisabled}>Lookup</button>
           </div>
         </div>
+        {isLoading && <h2 className='loading'>Loading...</h2>}
+        {isError && <h2 className='error'>User not found</h2>}
         {isReady &&
         <div className="row content">
           <div className='col-1-of-2'>
@@ -63,15 +89,18 @@ function App () {
               </a>
             }
             <p>
+              <span className='icon'><FontAwesomeIcon icon={faHashtag} /></span>
               <strong>User ID: </strong>
               <span className='userid'>{discordUser.id}</span>
             </p>
             <p>
+              <span className='icon'><FontAwesomeIcon icon={faIdBadge} /></span>
               <strong>Username: </strong>
               <span className='username'>{discordUser.username}</span>
             </p>
             {discordUser.badges.length !== 0 &&
               <p>
+                <span className='icon'><FontAwesomeIcon icon={faTags} /></span>
                 <strong>Badge: </strong>
                 {discordUser.badges.map((badge, key) => {
                   return <span key={key}><img height="20" className='badge' src={`/img/badges/${badge}.svg`} alt={badge}/></span>
@@ -79,13 +108,15 @@ function App () {
               </p>
             }
             <p>
+              <span className='icon'><FontAwesomeIcon icon={faStar} /></span>
               <strong>Created: </strong>
               <span className='created'>{discordUser.creationDate}</span>
             </p>
             {discordUser.bannerColor &&
               <p>
+                <span className='icon'><FontAwesomeIcon icon={faPalette} /></span>
                 <strong>Banner Color: </strong>
-                <span className='bannerColor' style={{color: discordUser.bannerColor, backgroundColor: discordUser.bannerColor}}>{discordUser.bannerColor}</span>
+                <span className='bannerColor' style={{color: discordUser.bannerColor}}>{discordUser.bannerColor}</span>
               </p>
             }
           </div>
