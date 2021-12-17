682ed34b7d6822758f141869aa0a22752922e6c7
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-12-17 19:10:17 +0100

    Update footer design

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -2,15 +2,13 @@ import React from 'react'
 
 export default function AppFooter() {
     return (
-        <footer>
-            <div className='box'>
+        <footer class="site-footer">
                 <span>DISCLAIMER: I do not save anything and this website is not affiliated with </span>
-                <a className='footer-link' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord.</a>
+                <a className='footer-links' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord.</a>
                 <div>
                     <span>Feel free to contact me on Discord if you have any questions: </span>
-                    <a className='footer-link' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Ayuki#0001</a>
+                    <a className='footer-links' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Ayuki#0001</a>
                 </div>
-            </div>
         </footer>
     )
 }
