2ac952d2f82109c9155daa849ae66ab530387e60
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-12-17 18:36:59 +0100

    Update footer box

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -3,11 +3,13 @@ import React from 'react'
 export default function AppFooter() {
     return (
         <footer>
-            <span>DISCLAIMER: I do not save anything and this website is not affiliated with </span>
-            <a className='footer-link' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord.</a>
-            <div>
-                <span>Feel free to contact me on Discord if you have any questions: </span>
-                <a className='footer-link' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Ayuki#0001</a>
+            <div className='box'>
+                <span>DISCLAIMER: I do not save anything and this website is not affiliated with </span>
+                <a className='footer-link' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord.</a>
+                <div>
+                    <span>Feel free to contact me on Discord if you have any questions: </span>
+                    <a className='footer-link' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Ayuki#0001</a>
+                </div>
             </div>
         </footer>
     )
