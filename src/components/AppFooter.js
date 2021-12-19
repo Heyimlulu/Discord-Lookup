61860cf73696be87343b73d46f97f1b24d32180f
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2021-12-19 22:10:34 +0100

    Update AppFooter.js

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -3,8 +3,12 @@ import React from 'react'
 export default function AppFooter() {
     return (
         <footer class="site-footer">
-                <span>DISCLAIMER: I do not save anything and this website is not affiliated with </span>
-                <a className='footer-links' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord.</a>
+                <div>
+                    I do not save anything: <a href='https://github.com/Heyimlulu/Discord-Lookup' target="_blank" rel="noopener noreferrer">See sources</a>
+                </div>
+                <div>
+                    This website is not affiliated with <a className='footer-links' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord</a>
+                </div>
                 <div>
                     <span>Feel free to contact me on Discord if you have any questions: </span>
                     <a className='footer-links' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Ayuki#0001</a>
