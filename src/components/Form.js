88cf8ef7e1148dc20baf9f90f02bb6f20b00a219
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-01-22 17:00:02 +0100

    remove gray background on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,11 +6,11 @@ import { faChevronCircleRight, faQuestionCircle } from '@fortawesome/free-solid-
 export default function Form({handleChange, userID, handleClick, isDisabled, isLoading}) {
     return (
         <div className="row">
-            <form className="box">
+            <form>
                 <div className="flex">
                     <label htmlFor='userid-input' className="userid-label">User ID / Bot ID:</label>
                     <a className="links" href="https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-" target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon icon={faQuestionCircle} />
+                        Learn more
                     </a>
                 </div>
                 <input type="text" name="userid-input" id="userid-input" className="userid-input" maxLength={22} onChange={handleChange} value={userID} />
