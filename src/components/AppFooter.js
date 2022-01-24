ef9e4e3c339bb1c9619836d5c16eb4dcc3adb2c1
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-01-24 17:32:46 +0100

    Update AppFooter.js

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -11,7 +11,7 @@ export default function AppFooter({ visits }) {
                     <span>Numbers of search today: <span className="count">{ visits }</span></span>
                     <div>
                         <span>Feel free to contact me on Discord if you have any questions: </span>
-                        <a className='footer-links' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Ayuki#0001</a>
+                        <a className='footer-links' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Lulu 🍉#6969</a>
                     </div>
                 </div>
         </footer>
