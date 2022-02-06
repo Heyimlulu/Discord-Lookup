0e4a24f8d68510be7cd8d91f6711285020ab2403
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-02-06 14:56:25 +0100

    Update three dots loader

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -19,24 +19,6 @@ export default function AppFooter({ visits }) {
                             <a className='text-blurple transition-all pl-0 hover:opacity-70' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Lulu 🍉#6969</a>
                         </p>
                     </div>
-
-                    {/*
-                    <div>
-                        This website is not affiliated with <a className='footer-links' href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord.</a>
-                    </div>
-                    <div>
-                        Checkout my others Discord related projects: &nbsp;
-                        <a className='footer-links' href='https://github.com/Heyimlulu/Paanya' target="_blank" rel="noopener noreferrer">Paanya</a>
-                        <span> & </span>
-                        <a className='footer-links' href='https://github.com/Heyimlulu/SlashBot' target="_blank" rel="noopener noreferrer">Ottism</a>
-                        <span> - </span>
-                        Numbers of lookups today: <span className="count">{ visits }</span>
-                    </div>
-                    <div>
-                        <span>Feel free to contact me on Discord if you have any questions: </span>
-                        <a className='footer-links' href='https://discord.com/users/265896171384340480' target="_blank" rel="noopener noreferrer">Lulu 🍉#6969</a>
-                    </div>
-                    */}
                 </div>
         </footer>
     )
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,24 +3,26 @@ import Loader from "react-loader-spinner";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faQuestionCircle, faSearch} from '@fortawesome/free-solid-svg-icons';
 
+import '../three-dots.css';
+
 export default function Form({handleChange, userID, handleClick, isDisabled, isLoading}) {
     return (
         <div className="py-8 sm:py-16">
             <div className="mx-auto">
-                <div className="relative rounded-2xl px-6 py-10 bg-indigo-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+                <div className="relative rounded-2xl px-6 py-10 overflow-hidden sm:px-12 sm:py-20">
                     <div className="relative">
-                        <div className="sm:text-center">
+                        <div className="text-center">
                             <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
                                 <FontAwesomeIcon className="mr-2" icon={faSearch} />
                                 Discord Lookup
                             </h2>
-                            <p className="mt-6 mx-auto max-w-2xl text-lg text-indigo-200">
+                            <p className="mt-6 mx-auto max-w-2xl text-lg text-white">
                                 Lookup for a Discord User or Bot ID
                             </p>
                         </div>
                         <div className="relative sm:mx-auto sm:max-w-xs mt-4 -mb-6">
                             <a className="absolute top-4 right-2 sm:right-16 px-2 py-1.5" href="https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-" target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon className="transition-all text-grey hover:opacity-60" icon={faQuestionCircle} />
+                                <FontAwesomeIcon className="transition-all text-blue-500 hover:text-blue-400" icon={faQuestionCircle} />
                             </a>
                         </div>
                         <div className="mt-12 sm:mx-auto sm:max-w-lg sm:flex">
@@ -41,11 +43,11 @@ export default function Form({handleChange, userID, handleClick, isDisabled, isL
                             <div className="mt-4 sm:mt-0 sm:ml-3">
                                 <button
                                     type="submit"
-                                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10 transition-all disabled:opacity-60 font-bold cursor-pointer"
+                                    className="block h-full w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10 transition-all disabled:opacity-60 font-bold cursor-pointer"
                                     onClick={handleClick}
                                     disabled={isDisabled}
                                 >
-                                    {isLoading ? <Loader type="ThreeDots" color="#FFFFFF" height={25} width={40} /> : <span>Lookup</span>}
+                                    {isLoading ? <div className="mx-auto my-2 sm:my-0 dot-flashing"></div> : <span>Lookup</span>}
                                 </button>
                             </div>
                         </div>
