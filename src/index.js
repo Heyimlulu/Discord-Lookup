78f75fdfbdeefc69ba765ecf077ce23c1e34e3b5
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-04-24 11:50:47 +0200

    SSR

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import './index.css';
-import App from './App';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './utils/i18n';
 import * as gtag from './utils/gtag';
@@ -10,10 +9,12 @@ import * as gtag from './utils/gtag';
 // Google Analytics
 ReactGA.initialize(gtag.GA_TRACKING_ID);
 
+const AppRoot = lazy(() => import('./App'));
+
 ReactDOM.render(
     <I18nextProvider i18n={i18n}>
         <Suspense fallback={<div className='text-2xl font-bold text-blurple text-center translate-y-1/2'>Loading...</div>}>
-            <App />
+            <AppRoot />
         </Suspense>
     </I18nextProvider>,
     document.getElementById('root')
