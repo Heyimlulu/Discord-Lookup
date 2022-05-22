31d9e20f39e1208b9163d744a6a67e7160a17e16
Lucas <61554619+Heyimlulu@users.noreply.github.com>
2022-05-22 20:59:41 +0200

    Adding Service Worker for PWA

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from './utils/i18n';
 import * as gtag from './utils/gtag';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // Google Analytics
 ReactGA.initialize(gtag.GA_TRACKING_ID);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 createRoot(document.getElementById('root')).render(
     <I18nextProvider i18n={i18n}>
